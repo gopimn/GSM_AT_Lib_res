335692d4bb971cf282170bbbb7ec6c8d8d2935d0
tilz0R <tilen.majerle@gmail.com>
2018-12-15 02:43:36 +0100

    Add latest lib + MQTT test code

--- dev/VisualStudio/gsm_dev_os/main.c
+++ dev/VisualStudio/gsm_dev_os/main.c
@@ -5,6 +5,9 @@
 #include "windows.h"
 #include "gsm/gsm.h"
 
+#include "gsm/apps/gsm_mqtt_client_api.h"
+#include "gsm/gsm_mem.h"
+
 static void main_thread(void* arg);
 DWORD main_thread_id;
 
@@ -59,6 +62,112 @@ uint8_t request_data[] = ""
 "\r\n";
 
 /**
+ * \brief           Client ID is structured from ESP station MAC address
+ */
+static char
+mqtt_client_id[13];
+
+/**
+ * \brief           Connection information for MQTT CONNECT packet
+ */
+static const gsm_mqtt_client_info_t
+mqtt_client_info = {
+    /* Server login data */
+    .user = "8a215f70-a644-11e8-ac49-e932ed599553",
+    .pass = "26aa943f702e5e780f015cd048a91e8fb54cca28",
+
+    /* Device identifier address */
+    .id = "b24908a0-a652-11e8-af33-cfeebe848e6e",
+
+    .keep_alive = 10,
+};
+
+/**
+ * \brief           MQTT client API thread
+ */
+void
+mqtt_client_api_thread(void const* arg) {
+    gsm_mqtt_client_api_p client;
+    gsm_mqtt_conn_status_t conn_status;
+    gsm_mqtt_client_api_buf_p buf;
+    gsmr_t res;
+
+    /* Create new MQTT API */
+    client = gsm_mqtt_client_api_new(256, 128);
+    if (client == NULL) {
+        goto terminate;
+    }
+
+    while (1) {
+        /* Make a connection */
+        printf("Joining MQTT server\r\n");
+
+        /* Try to join */
+        conn_status = gsm_mqtt_client_api_connect(client, "mqtt.mydevices.com", 1883, &mqtt_client_info);
+        if (conn_status == GSM_MQTT_CONN_STATUS_ACCEPTED) {
+            printf("Connected and accepted!\r\n");
+            printf("Client is ready to subscribe and publish to new messages\r\n");
+        } else {
+            printf("Connect API response: %d\r\n", (int)conn_status);
+            gsm_delay(5000);
+            continue;
+        }
+
+        /* Subscribe to topics */
+        if (gsm_mqtt_client_api_subscribe(client,
+            "v1/8a215f70-a644-11e8-ac49-e932ed599553/things/b24908a0-a652-11e8-af33-cfeebe848e6e/cmd/#",
+            GSM_MQTT_QOS_AT_LEAST_ONCE) == gsmOK) {
+            printf("Subscribed to esp8266_mqtt_topic\r\n");
+        } else {
+            printf("Problem subscribing to topic!\r\n");
+        }
+
+        while (1) {
+            /* Receive MQTT packet with 1000ms timeout */
+            res = gsm_mqtt_client_api_receive(client, &buf, 1000);
+            if (res == gsmOK) {
+                if (buf != NULL) {
+                    const char* payload;
+                    printf("Publish received!\r\n");
+                    printf("Topic: %s, payload: %s\r\n", buf->topic, buf->payload);
+
+                    /* Check for light switch */
+                    if ((strstr(buf->topic, "/cmd/25")) != NULL) {
+                        payload = strstr((void *)buf->payload, ",");
+                        if (payload != NULL) {
+                            payload++;
+                            if (*payload == '1') {
+                                printf("RELAY ENABLED!\r\n");
+                            } else {
+                                printf("RELAY DISABLED!\r\n");
+                            }
+
+                            /* Format topic name */
+                            gsm_mqtt_client_api_publish(client, 
+                                "v1/8a215f70-a644-11e8-ac49-e932ed599553/things/b24908a0-a652-11e8-af33-cfeebe848e6e/data/25",
+                                payload, 1, GSM_MQTT_QOS_AT_LEAST_ONCE, 1);
+                        }
+                    }
+                    gsm_mqtt_client_api_buf_free(buf);
+                    buf = NULL;
+                }
+            } else if (res == gsmCLOSED) {
+                printf("MQTT connection closed!\r\n");
+                break;
+            } else if (res == gsmTIMEOUT) {
+                /* Receive timeout */
+                /* printf("Timeout\r\n"); */
+            }
+        }
+    }
+
+terminate: 
+    gsm_mqtt_client_api_delete(client);
+    printf("MQTT client thread terminate\r\n");
+    gsm_sys_thread_terminate(NULL);
+}
+
+/**
  * \brief           Main thread for init purposes
  */
 static void
@@ -100,6 +209,12 @@ main_thread(void* arg) {
     }
 
 #if GSM_CFG_NETCONN
+
+    gsm_sys_thread_create(NULL, "mqtt_thread", (gsm_sys_thread_fn)mqtt_client_api_thread, NULL, GSM_SYS_THREAD_SS, GSM_SYS_THREAD_PRIO);
+    while (1) {
+        gsm_delay(100000);
+    }
+
     nc = gsm_netconn_new(GSM_NETCONN_TYPE_TCP);
     if (nc != NULL) {
         if (gsm_netconn_connect(nc, "example.com", 80) == gsmOK) {
