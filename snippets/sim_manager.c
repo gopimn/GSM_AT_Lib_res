41966afc3ceafc8d3c303e411cf8b3c3cbeee944
tilz0R <tilen.majerle@gmail.com>
2018-12-16 20:53:49 +0100

    Add snippets for examples code

---
+++ snippets/sim_manager.c
@@ -0,0 +1,29 @@
+#include "sim_manager.h"
+#include "gsm/gsm.h"
+
+/**
+ * \brief           SIM card pin code
+ */
+const char *
+pin_code = "1234";
+
+/**
+ * \brief           SIM card puk code
+ */
+const char *
+puk_code = "";
+
+/**
+ * \brief           Configure and enable SIM card
+ * \return          `1` on success, `0` otherwise
+ */
+uint8_t
+configure_sim_card(void) {
+    if (pin_code != NULL && strlen(pin_code)) {
+        if (gsm_sim_pin_enter(pin_code, 1) == gsmOK) {
+            return 1;
+        }
+        return 0;
+    }
+    return 1;
+}
