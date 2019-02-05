a3d81a8b8206419691e34f41478f9e9524adf7fa
tilz0R <tilen.majerle@st.com>
2019-02-05 11:48:21 +0100

    Fix system clock config function

--- examples/stm32f429zi-nucleo/sms_send_receive_rtos/src/main.c
+++ examples/stm32f429zi-nucleo/sms_send_receive_rtos/src/main.c
@@ -210,30 +210,36 @@ LL_Init(void) {
  */
 void
 SystemClock_Config(void) {
+	/* Configure flash latency */
     LL_FLASH_SetLatency(LL_FLASH_LATENCY_5);
-
     if (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_5) {
         while (1) {}
     }
+	
+	/* Set voltage scaling */
     LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
     LL_PWR_DisableOverDriveMode();
+	
+	/* Enable HSE */
     LL_RCC_HSE_EnableBypass();
     LL_RCC_HSE_Enable();
-
-    /* Wait till HSE is ready */
     while (LL_RCC_HSE_IsReady() != 1) {}
+	
+    /* Configure PLL */
     LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_4, 168, LL_RCC_PLLP_DIV_2);
     LL_RCC_PLL_Enable();
-
-    /* Wait till PLL is ready */
     while (LL_RCC_PLL_IsReady() != 1) {}
+	
+	/* Set prescalers */
     LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
     LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_4);
     LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_2);
+	
+    /* Configure system clock */
     LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-
-    /* Wait till System clock is ready */
     while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL) {}
+	
+    /* Configure systick */
     LL_Init1msTick(168000000);
     LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
     LL_SYSTICK_EnableIT();
@@ -254,6 +260,7 @@ USART_Printf_Init(void) {
 
     /*
      * USART3 GPIO Configuration
+	 *
      * PD8   ------> USART3_TX
      * PD9   ------> USART3_RX
      */
